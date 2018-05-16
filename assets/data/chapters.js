37189d09964ce9aa1f3268a5b4886ad8ca5dd6f3
Fidel Thomet <me@fidelthomet.com>
2018-05-16 16:04:49 +0200

    [Update] added data

---
+++ assets/data/chapters.js
@@ -0,0 +1,10 @@
+export default [{
+  title: 'Climate Change Scenario Primer',
+  route: '/'
+}, {
+  title: 'It starts with socioeconomics'
+}, {
+  title: 'The SSP-RCP Scenario Framework'
+}, {
+  title: 'Scenarios and Models'
+}]
