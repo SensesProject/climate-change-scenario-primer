235ab876954ec3190b1e337c9b887d98f55dd8da
Fidel Thomet <me@fidelthomet.com>
2018-05-16 16:39:45 +0200

    [Update] dynamic page header

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="LayoutHeader">
-    LayoutHeader
+  <div
+    :class="{index}"
+    class="LayoutHeader">
+    <section class="wrapper">
+      <header>
+        <h1>SENSES</h1>
+      </header>
+      <h2>{{ title }}</h2>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    title () {
+      const { $store, $route } = this
+      return $store.getters.chapters.find(chapter => chapter.route === $route.path).title
+    },
+    index () {
+      return this.$route.path === '/'
+    }
+  }
 }
 </script>
 
@@ -17,7 +32,18 @@ export default {
   @include flex-column();
   background: $color-violet;
   color: $color-white;
+  height: 30vh;
 
-  height: 25vh;
+  &.index {
+    height: 60vh;
+  }
+
+  .wrapper {
+    height: 100%;
+    padding: $spacing-unit / 2;
+    @include flex-column();
+    align-items: flex-start;
+    justify-content: space-between;
+  }
 }
 </style>
