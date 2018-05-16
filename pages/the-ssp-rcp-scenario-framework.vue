ab6e0dd5f14c4ac876ae0e786dfe2bb5dac30549
Fidel Thomet <me@fidelthomet.com>
2018-05-16 16:07:47 +0200

    [Update] added pages for chapters

---
+++ pages/the-ssp-rcp-scenario-framework.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="wrapper">
+    socioeconomics
+  </section>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  /* min-height: 100vh; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
