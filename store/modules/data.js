939de3dd6cdf80e4c892bf9c9b8e9d49059ad0c8
Fidel Thomet <me@fidelthomet.com>
2018-05-16 16:05:06 +0200

    [Update] added store

---
+++ store/modules/data.js
@@ -0,0 +1,17 @@
+import data from '~/assets/data'
+
+const state = () => (data)
+
+const getters = {
+  chapters (state) {
+    return state.chapters.map(({ title, route }) => {
+      if (route !== undefined) return { title, route }
+      return {
+        title,
+        route: title.toLowerCase().replace(/ /g, '-')
+      }
+    })
+  }
+}
+
+export default { state, getters }
