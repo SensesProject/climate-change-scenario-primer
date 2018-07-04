895653dc95d40caa408cf172d7d3f72434517139
0xE <fidelthomet@Fidels-MacBook-Pro.local>
2018-07-04 11:06:54 +0200

    [Content] climate & climate impacts

--- pages/climate-impacts.vue
+++ pages/climate-impacts.vue
@@ -3,80 +3,80 @@
 
     <h2>Climate Impacts</h2>
 
-    <p>Climate impacts are consequences and effects of climate change on nature and society. These may include changed agricultural yields, increased flooding and droughts, and coral reef bleaching due to changes in the chemical composition of the ocean. The Inter-Sectoral Impact Model Intercomparison Project (ISIMIP) plays an important role in climate-impact research and covers currently 13 sectors.</p>
+    <p>Climate impacts are consequences and effects of climate change on nature and society. These may include changed agricultural yields, increased floods and droughts, or coral reef bleaching due to changes in the chemical composition of the ocean. The Inter-Sectoral Impact Model Intercomparison Project (ISIMIP) plays an important role in climate-impact research by faciliting consistent impact simulations across various sectors and collecting and disseminating the results.</p>
 
     <LayoutCards>
 
       <LayoutCard>
         <h4 slot="title">Agriculture</h4>
-        <p>Text describing Agriculture</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Biodiversity</h4>
-        <p>Text describing Biodiversity</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Biomes</h4>
-        <p>Text describing Biomes</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Coastal Infrastructure</h4>
-        <p>Text describing Coastal Infrastructure</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Energy</h4>
-        <p>Text describing Energy</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Forestry</h4>
-        <p>Text describing Forestry</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Health</h4>
-        <p>Text describing Health</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Lakes</h4>
-        <p>Text describing Lakes</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Marine Fishery</h4>
-        <p>Text describing Marine Fishery</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Permafrost</h4>
-        <p>Text describing Permafrost</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Tropical Cyclones</h4>
-        <p>Text describing Tropical Cyclones</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Water (global)</h4>
-        <p>Text describing Water (global)</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Water (regional)</h4>
-        <p>Text describing Water (regional)</p>
+        <p>Sector description coming soon</p>
       </LayoutCard>
 
     </LayoutCards>
 
-    <p>Of course there is a strong link between those sectors and changes in climate, but for some of them changing socioeconomic factors also play an important role. That&rsquo;s why the SSP-RCP Framework is also used here. However, climate impact scenarios generally don&rsquo;t cover the whole bandwith of the SSP-RCP framework. Most of them refer to SSP2 and either RCP 2.6 or RCP 6.0.</p>
+    <p>In the anthropocene humans are the driver of many processes, therefore socioeconomic factors also play a major role in climate impact research. Because of that not only climate scenarios belonging to a certain RCP, but also SSP  scenarios are input to the impact models. Currently only the SSP2 scenario is used, though.</p>
 
-    <p>Climate impact models – similarly to climate models – produce gridded data, which can be shown in maps. However, another way of looking at that data is by showing the relation of temperature changes and changes in affected land area.</p>
+    <p>The climate impact models – similarly to climate models – produce maps of impacts.</p>
 
     <LayoutColumns>
       <VisChartImpact
@@ -87,10 +87,11 @@
         variable="Crop Failure"/>
     </LayoutColumns>
 
-    <p>The charts show the simulation results for floods and crop failure. In each case, climate data from three different global climate models <strong class="green">GFDL-ESM2M</strong>, <strong class="yellow">MIROC5</strong> and <strong class="violet">IPSL-CM5A-LR</strong> were used to drive the impact models – each color relates to results using data from a different climate model. The thick lines depict the median of the ensemble for all impact models considered. The shaded areas show the 20th to 80th percentile of that ensemble.</p>
+    <p>The charts above show simulation analyses of impact simulations for two sectors, floods and crop failure. In particular, the change in the percentage of the global surface that is affected by extreme event when compared to preindustrial times, is shown. In each case, climate data from three different global climate models (<strong class="green">GFDL-ESM2M</strong>, <strong class="yellow">MIROC5</strong> and <strong class="violet">IPSL-CM5A-LR</strong>) were used to drive the impact models – each color relates to results using data from a different climate model. The thick lines depict the median of the ensemble for all impact models considered. The shaded areas show the 20th to 80th percentile of that ensemble and thus give a feel of the model spread.</p>
 
     <LayoutRecap>
-      <li>a</li>
+      <li>There is a wide range of different climate impacts.</li>
+      <li>Climate impact models use data from climate and integrated assessment models to create impact scenarios.</li>
     </LayoutRecap>
     <LayoutNextChapter/>
   </section>
--- pages/climate.vue
+++ pages/climate.vue
@@ -2,7 +2,7 @@
   <section class="wrapper">
     <h2>Climate</h2>
 
-    <p>Climate models take us from emissions to things like temperature and precipitation. As with IAMs they describe a complex system,  in this case the earth. Climate models are therefore sometimes also related to as Earth System Models. In contrast to weather models they focus on long term changes whereas spatial and temporal resolution is less important.</p>
+    <p>Climate models translate greenhouse gas (GHG) concentrations in the atmosphere to meteorological quantities such as temperature or precipitation. It turns out that many more systems than just the atmosphere decisively influence the climate. Therefore more and more subsystems, e.g. oceans, vegetation or sea ice, had to be added to climate models. Because of that these models are also known as Earth System Models.</p>
 
     <span>Change in average near surface temperature (2006-2010 to 2095-2099)</span>
     <LayoutColumns>
@@ -14,10 +14,10 @@
         scenario="rcp60"/>
     </LayoutColumns>
 
-    <p>This shows the change in average near surface temperature from 2006-2010 to 2095-2099 for two different emission scenarios: RCP 2.6 and RCP 6.0. As you can see: the overall temperature is going to raise, the intensity varies between regions and the temperature changes in the RCP 6.0 scenario are far more severe.</p>
+    <p>The above plot shows the change in average near surface temperature from 2006-2010 to 2095-2099 for two different GHG scenarios: RCP 2.6 and RCP 6.0. As you can see the overall temperature is going to raise, the intensity varies between regions and the temperature changes in the RCP 6.0 scenario are far more severe.</p>
 
     <LayoutRecap>
-      <li>a</li>
+      <li>Climate models use emission scenarios to create climate scenarios.</li>
     </LayoutRecap>
     <LayoutNextChapter/>
   </section>
