43b2362ab1eaf0e7e8a48c5fbb56ef8f59d86e15
0xE <fidelthomet@Fidels-MacBook-Pro.local>
2018-07-05 11:39:04 +0200

    [Content] climate impacts sector descriptions

--- pages/climate-impacts.vue
+++ pages/climate-impacts.vue
@@ -9,7 +9,7 @@
 
       <LayoutCard>
         <h4 slot="title">Agriculture</h4>
-        <p>Sector description coming soon</p>
+        <p>The agriculture sector in ISIMIP seeks to understand how crops will be affected by changing atmospheric composition and climate. Increasing carbon dioxide concentrations may help some crops perform better, and in some regions increased temperatures could extend the growing season. But in other regions, more frequent and more intense heat waves and droughts could harm crop yields. This sector also examines how changing management practices (crop varieties, fertiliser, and irrigation) could be used to counteract negative effects of climate change.</p>
       </LayoutCard>
 
       <LayoutCard>
@@ -19,12 +19,12 @@
 
       <LayoutCard>
         <h4 slot="title">Biomes</h4>
-        <p>Sector description coming soon</p>
+        <p>The Global Biomes Sector studies how climate and land-use change will affect the distribution and functioning of natural vegetation around the globe. Especially carbon and water cycling as well as the biogeographic distribution of plants under climate change are particularly relevant for this sector. The simulations contribute to better understand the capacity of natural vegetation to sequester and store carbon under increasing pressure from climate change but also from land-use changes.</p>
       </LayoutCard>
 
       <LayoutCard>
         <h4 slot="title">Coastal Infrastructure</h4>
-        <p>Sector description coming soon</p>
+        <p>The increase of damage due to coastal extreme sea-level events (from tides, mean sea level anomalies, tropical cyclones, extratropical storm surges and waves) may be one of the costliest aspects of climate change and sea-level rise. The coastal sector of ISIMIP assesses these impacts at global and continental scales in order to inform a range of policy decisions including setting global mitigation targets, designing global financial mechanisms for adaptation and loss & damage, and strategic long-term coastal adaptation planning. In order to adequately inform such decisions, we systematically consider all major uncertainties in assessments of current and future coastal risk, including climate and socio-economic scenario uncertainty, climate model and impact uncertainty, natural internal variability, as well uncertainties methods applied for extreme value analysis.</p>
       </LayoutCard>
 
       <LayoutCard>
@@ -34,7 +34,7 @@
 
       <LayoutCard>
         <h4 slot="title">Forestry</h4>
-        <p>Sector description coming soon</p>
+        <p>The Regional Forest Sector strives to understand how climate change will affect managed forests. Currently, simulations are carried out at the forest stand level for a wide range forest types covering the main forest tree species in Europe. The simulations are designed to disentangle the effects of different management schemes, of changing CO2-concentrations and of different climate scenario on key forest services and functions such as plant productivity and growth, wood harvests and carbon and water cycling. Climate change is impacting forests in manifold ways potentially increasing productivity unless water is limited or disturbances such as fire or insect outbreaks lead to catastrophic damages.</p>
       </LayoutCard>
 
       <LayoutCard>
@@ -44,12 +44,12 @@
 
       <LayoutCard>
         <h4 slot="title">Lakes</h4>
-        <p>Sector description coming soon</p>
+        <p>Analyses in the ISIMIP lake sector currently focus  on how climate change is affecting the physical properties of lake functioning. Lake sector simulations follow two synergetic tracks, whereby one-dimensional lake models are either (i) tailored to reproduce observed processes in a selection of around 50 well-studied lakes, or (ii) run across all lake regions of the globe with one consistent set-up. The resulting data set provides an unprecedented opportunity to assess lake model performance and uncover global warming impacts on e.g. thermal stratification or lake ice dynamics, thereby forming the basis for understanding how lacustrine systems will respond to anthropogenic climate change.</p>
       </LayoutCard>
 
       <LayoutCard>
-        <h4 slot="title">Marine Fishery</h4>
-        <p>Sector description coming soon</p>
+        <h4 slot="title">Marine Fishery (FishMIP)</h4>
+        <p>FishMIP looks at the future of marine ecosystems, and consequences for fisheries, biodiversity, and food supply. Climate change impacts the oceans in many ways, such as warming, ocean acidification, and deoxygenation, and we have a range of models to explore these issues with. The oceans provide many services to human beings, and the health and productivity of marine ecosystems remains of critical importance. Marine ecosystem models are extremely varied, so FishMIP allows us to look at these questions and make projections from many different angles.</p>
       </LayoutCard>
 
       <LayoutCard>
