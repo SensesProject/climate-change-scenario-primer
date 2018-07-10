8df849ecbc802b4e8b7fe3dac78cdb6077094e5d
0xE <fidelthomet@Fidels-MacBook-Pro.local>
2018-07-10 11:27:35 +0200

    [Update] feedback template

--- components/LayoutBanner.vue
+++ components/LayoutBanner.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="LayoutBanner">
     <span>Beta Version</span>
-    <a href="mailto:senses-project@fh-potsdam.de?subject=Primer Feedback&body=Did you like our website?"><span>→ Provide Feedback!</span></a>
+    <a :href="`mailto:thomet@fh-potsdam.de?subject=Primer Feedback&body=${template}`"><span>→ Provide Feedback!</span></a>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      template: `Thanks for your interest in the Climate Change Scenarios Primer and for sharing Feedback with us. You can give us your feedback in any form you like - but you can use this outline for any comments and suggestions:
 
+# Usefulness
+- Please rate the overall usefulness of the Primer!
+ (0: not useful at all, 10: extremely useful)
+- What would make the primer more useful for you?
+
+# Content
+- Which parts of the Primer do you like most and least?
+- Is anything missing or unclear?
+
+# Context
+- In which way and in which context will you be using the primer?
+- Do you use the primer to educate others and in which way?
+
+# Format
+- Is the length of the primer good? Should it be shorter or longer?
+- Which parts could be shorter or longer?`.replace(/\n/g, '%0D%0A')
+    }
+  }
 }
 </script>
 
