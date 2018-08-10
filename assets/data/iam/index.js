6f20c3b78f8bea6e8f008ffed957b5258789d99c
Fidel Thomet <fidelthomet@Fidels-MacBook-Pro.local>
2018-08-10 19:28:33 +0200

    [Update] mobile friendly and overall improved model structure: reolves #8

--- assets/data/iam/index.js
+++ assets/data/iam/index.js
@@ -38,46 +38,46 @@ const ec = variables.find(v => v.variable === 'ec')
 ed.max = ec.max = Math.max(ed.max, ec.max)
 
 const elements = [{
-  name: 'Investments',
-  variable: 'i',
-  x: 2,
-  y: 0,
-  color: 'green',
-  minStep: 1
-}, {
-  name: 'Dirty Energy',
-  variable: 'ied',
-  x: 1.3,
-  y: 1,
-  color: 'green',
-  minStep: 2
-}, {
-  name: 'Clean Energy',
-  variable: 'iec',
-  x: 2.3,
-  y: 1,
-  color: 'green',
-  minStep: 2
-}, {
-  name: 'Dirty Energy',
-  variable: 'ed',
-  x: 1.3,
-  y: 2,
-  color: 'yellow',
-  minStep: 2
-}, {
-  name: 'Clean Energy',
-  variable: 'ec',
-  x: 2.3,
-  y: 2,
-  color: 'yellow',
-  minStep: 2
-}, {
+//   name: 'Investments',
+//   variable: 'i',
+//   x: 2,
+//   y: 0,
+//   color: 'blue',
+//   minStep: 1
+// }, {
+//   name: 'Dirty Energy',
+//   variable: 'ied',
+//   x: 1.3,
+//   y: 1,
+//   color: 'blue',
+//   minStep: 2
+// }, {
+//   name: 'Clean Energy',
+//   variable: 'iec',
+//   x: 2.3,
+//   y: 1,
+//   color: 'blue',
+//   minStep: 2
+// }, {
+//   name: 'Dirty Energy',
+//   variable: 'ed',
+//   x: 1.3,
+//   y: 2,
+//   color: 'yellow',
+//   minStep: 2
+// }, {
+//   name: 'Clean Energy',
+//   variable: 'ec',
+//   x: 2.3,
+//   y: 2,
+//   color: 'yellow',
+//   minStep: 2
+// }, {
   name: 'Population',
   variable: 'l',
   x: 0.5,
-  y: 3,
-  color: 'blue',
+  y: 0,
+  color: 'green',
   dash: {
     in: false
   }
@@ -85,7 +85,7 @@ const elements = [{
   name: 'Energy',
   variable: 'e',
   x: 1.5,
-  y: 3,
+  y: 0,
   color: 'yellow',
   dash: {
     in: 1
@@ -94,8 +94,8 @@ const elements = [{
   name: 'Capital Stock',
   variable: 'k',
   x: 2.5,
-  y: 3,
-  color: 'violet',
+  y: 0,
+  color: 'blue',
   dash: {
     in: 1
   }
@@ -103,28 +103,114 @@ const elements = [{
   name: 'GDP',
   variable: 'y',
   x: 1.5,
-  y: 4,
-  color: 'violet',
-  dash: {
-    out: 1
-  }
+  y: 1,
+  color: 'blue'
 }, {
   name: 'Consumption',
   variable: 'c',
-  x: 2,
-  y: 5,
-  color: 'violet',
-  minStep: 1,
+  x: 0.5,
+  y: 2,
+  color: 'blue',
+  minStep: 0,
   dash: {
     out: false
   }
 }, {
+  name: 'Investments',
+  variable: 'i',
+  x: 2,
+  y: 2,
+  color: 'blue'
+}, {
+  name: 'Population',
+  variable: 'l',
+  x: 0.5,
+  y: 3,
+  color: 'green',
+  dash: {
+    in: false
+  }
+}, {
+  name: 'Energy',
+  variable: 'e',
+  x: 1.5,
+  y: 3,
+  color: 'yellow'
+}, {
+  name: 'Capital Stock',
+  variable: 'k',
+  x: 2.5,
+  y: 3,
+  color: 'blue'
+}, {
+  name: 'GDP',
+  variable: 'y',
+  x: 1.5,
+  y: 4,
+  color: 'blue'
+}, {
+  name: 'Consumption',
+  variable: 'c',
+  x: 0.5,
+  y: 5,
+  color: 'blue',
+  minStep: 0,
+  dash: {
+    out: false
+  }
+}, {
+  name: 'Investments',
+  variable: 'i',
+  x: 2,
+  y: 5,
+  color: 'blue'
+}, {
+  name: 'Population',
+  variable: 'l',
+  x: 0.5,
+  y: 7,
+  color: 'green',
+  dash: {
+    in: false
+  }
+}, {
+  name: 'Energy',
+  variable: 'e',
+  x: 1.5,
+  y: 7,
+  color: 'yellow'
+}, {
+  name: 'Capital Stock',
+  variable: 'k',
+  x: 2.5,
+  y: 7,
+  color: 'blue'
+}, {
+  name: 'GDP',
+  variable: 'y',
+  x: 1.5,
+  y: 8,
+  color: 'blue',
+  off: true
+}, {
+  name: 'Dirty Energy',
+  variable: 'ed',
+  x: 1,
+  y: 6,
+  color: 'yellow'
+}, {
+  name: 'Clean Energy',
+  variable: 'ec',
+  x: 2,
+  y: 6,
+  color: 'yellow'
+}, {
   name: 'Emissions',
   variable: 'ed',
   x: 0.5,
-  y: 5,
+  y: 8,
   color: 'red',
-  minStep: 2,
+  off: true,
   dash: {
     out: false
   }
@@ -132,68 +218,179 @@ const elements = [{
 
 const lines = [{
   points: [
+    [0.5, 1],
+    [1.5, 1]
+  ],
+  color: 'green'
+}, {
+  points: [
+    [1.5, 1],
+    [2.5, 1]
+  ],
+  color: 'blue'
+}, {
+  points: [
+    [1.5, 2],
+    ['max', 2],
+    ['max', 2, 'next'],
+    [0.5, 2, 'next']
+  ],
+  color: 'blue'
+}, {
+  points: [
+    [1.5, 3],
+    [2.5, 3]
+  ],
+  color: 'blue'
+}, {
+  points: [
     [0.5, 4],
     [1.5, 4]
   ],
-  color: 'blue'
+  color: 'green'
 }, {
   points: [
     [1.5, 4],
     [2.5, 4]
   ],
-  color: 'violet'
+  color: 'blue'
 }, {
   points: [
     [1.5, 5],
-    [2, 5]
-  ],
-  color: 'violet',
-  minStep: 1
-}, {
-  points: [
-    [2, 'min'],
-    ['max', 'min'],
     ['max', 5],
-    [2, 5]
+    ['max', 5, 'next']
   ],
-  color: 'green',
+  color: 'blue',
+  maxStep: 0
+}, {
+  points: [
+    ['max', 5, 'next'],
+    [0.5, 5, 'next']
+  ],
+  color: 'blue'
+}, {
+  points: [
+    [1, 6],
+    [2.5, 6],
+    [2.5, 7]
+  ],
+  color: 'blue'
+}, {
+  points: [
+    [1, 7],
+    ['min', 7],
+    ['min', 8, 'off'],
+    [0.5, 8, 'off']
+  ],
+  color: 'red'
+}, {
+  points: [
+    [1, 7],
+    [2, 7]
+  ],
+  color: 'yellow'
+}, {
+  points: [
+    [0.5, 8],
+    [1.5, 8]
+  ],
+  color: 'green'
+}, {
+  points: [
+    [1.5, 8],
+    [2.5, 8]
+  ],
+  color: 'blue'
+}, {
+  points: [
+    [1.5, 8],
+    [1.5, 8, 'off']
+  ],
+  color: 'blue'
+}, {
+  points: [
+    [1.5, 9, 'off'],
+    ['max', 9, 'off'],
+    ['max', 5, 'next']
+  ],
+  color: 'blue',
   minStep: 1
 }, {
   points: [
-    [1.5, 3],
-    [1.5, 1],
-    [2.5, 1],
-    [2.5, 3]
+    [1.5, 5],
+    ['max', 5],
+    ['max', 2, 'next']
   ],
-  color: 'green',
-  minStep: 1,
-  maxStep: 1
+  color: 'blue',
+  minStep: 1
 }, {
   points: [
-    [1.3, 1],
-    [2.9, 1],
-    [2.9, 3],
-    [2.5, 3]
+    [1.5, 3, 'off'],
+    [1.5, 3]
   ],
-  color: 'green',
-  minStep: 2
+  color: 'blue'
 }, {
   points: [
-    [1.3, 3],
-    [2.3, 3]
+    [1, 6, 'off'],
+    [1, 6],
+    [2, 6],
+    [2, 6, 'off']
   ],
-  color: 'yellow',
-  minStep: 2
-}, {
-  points: [
-    [1.3, 3],
-    ['min', 3],
-    ['min', 5],
-    [0.5, 5]
-  ],
-  color: 'red',
-  minStep: 2
-}]
+  color: 'blue'
+}
+
+// }, {
+//   points: [
+//     [1.5, 5],
+//     [2, 5]
+//   ],
+//   color: 'blue',
+//   minStep: 1
+// }, {
+//   points: [
+//     [2, 'min'],
+//     ['max', 'min'],
+//     ['max', 5],
+//     [2, 5]
+//   ],
+//   color: 'blue',
+//   minStep: 1
+// }, {
+//   points: [
+//     [1.5, 3],
+//     [1.5, 1],
+//     [2.5, 1],
+//     [2.5, 3]
+//   ],
+//   color: 'blue',
+//   minStep: 1,
+//   maxStep: 1
+// }, {
+//   points: [
+//     [1.3, 1],
+//     [2.9, 1],
+//     [2.9, 3],
+//     [2.5, 3]
+//   ],
+//   color: 'blue',
+//   minStep: 2
+// }, {
+//   points: [
+//     [1.3, 3],
+//     [2.3, 3]
+//   ],
+//   color: 'yellow',
+//   minStep: 2
+// }, {
+//   points: [
+//     [1.3, 3],
+//     ['min', 3],
+//     ['min', 5],
+//     [0.5, 5]
+//   ],
+//   color: 'red',
+//   minStep: 2
+]
 
 export default {
   modelOutput,
