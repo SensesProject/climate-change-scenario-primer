f20068f0dd853f5bd654cbed066600260cd42f05
Fidel Thomet <fidelthomet@Fidels-MacBook-Pro.local>
2018-08-23 14:08:16 +0200

    [Chore] removed unused code

--- components/VisScenarioTypes.vue
+++ components/VisScenarioTypes.vue
@@ -91,139 +91,15 @@
 import { mapState } from 'vuex'
 
 export default {
-  props: {
-    step: {
-      type: Number,
-      default: 0
-    }
-  },
   data () {
     return {
-      width: 0,
-      colors: ['#4E40B2', '#00A5D5', '#39C88A', '#FEAE00', '#C8005F'],
-      radius: 40,
-      terms: [
-        'Socioeconomics',
-        'Energy &<br>Land Use',
-        'Emissions',
-        'Climate',
-        'Climate Impacts'
-      ]
+      width: 0
     }
   },
   computed: {
     ...mapState([
       'view'
     ]),
-    items () {
-      const { terms, colors } = this
-      return terms.map((term, i) => {
-        const radius = 40
-        const circleRadius = 5
-        const arcOffset = 15
-        const cycleOffset = -72
-
-        const cycleOffsetRad = Math.PI * 2 * cycleOffset / 360
-        const arcOffsetRad = Math.PI * 2 * arcOffset / 360
-        const arcStartRad = cycleOffsetRad + arcOffsetRad
-        const arcEndRad = Math.PI * 2 / terms.length + cycleOffsetRad - arcOffsetRad
-
-        const start = {
-          x: Math.cos(cycleOffsetRad) * radius,
-          y: Math.sin(cycleOffsetRad) * radius
-        }
-        const arcStart = {
-          x: Math.cos(arcStartRad) * radius,
-          y: Math.sin(arcStartRad) * radius
-        }
-        const arcEnd = {
-          x: Math.cos(arcEndRad) * radius,
-          y: Math.sin(arcEndRad) * radius
-        }
-        const absoluteStart = {
-          x: Math.cos(cycleOffsetRad + Math.PI * 2 / terms.length * i) * radius,
-          y: Math.sin(cycleOffsetRad + Math.PI * 2 / terms.length * i) * radius
-        }
-        return {
-          term,
-          parent: {
-            transform: `rotate(${360 / terms.length * i})`
-          },
-          arc: {
-            d: `M ${arcStart.x} ${arcStart.y} A ${radius} ${radius} 0 0 1 ${arcEnd.x} ${arcEnd.y}`,
-            stroke: colors[i]
-          },
-          circle: {
-            cy: start.y,
-            cx: start.x,
-            r: circleRadius,
-            fill: colors[i]
-          },
-          arrowHead: {
-            transform: `translate(${arcEnd.x} ${arcEnd.y}) rotate(72)`,
-            d: 'M -2 -2 L 0 0 -2 2',
-            stroke: colors[i]
-          },
-          text: {
-            style: {
-              color: colors[i],
-              left: `${50 + circleRadius + absoluteStart.x}%`,
-              top: `${50 + absoluteStart.y}%`
-            }
-          }
-        }
-      })
-    },
-    circles () {
-      const { terms, radius } = this
-      return terms.map((term, i) => {
-        const offset = (Math.PI / terms.length) * (-2 + 2 * i)
-        const x = Math.cos(offset) * radius
-        const y = Math.sin(offset) * radius
-        return { term, x, y }
-      })
-    },
-    // items () {
-    //   const { circles, strokeWidth, step, fontSize } = this
-    //   return circles.map(({ term, x, y }, i) => {
-    //     let color = null
-    //     if (step < 5 && i === step) {
-    //       color = 'yellow'
-    //     }
-    //     if (step === 5) {
-    //       if (i === 0) color = 'green'
-    //       if (i === 2) color = 'blue'
-    //       if (i === 4) color = 'red'
-    //     }
-    //     if (step === 6) {
-    //       if (i <= 1) color = 'green'
-    //       if (i === 4) color = 'red'
-    //     }
-    //     if (step === 7) {
-    //       if (i <= 2) color = 'green'
-    //       if (i === 3) color = 'violet'
-    //       if (i === 4) color = 'red'
-    //     }
-    //     return {
-    //       term,
-    //       transform: `translate(${x}, ${y})`,
-    //       circle: {
-    //         r: 4,
-    //         'stroke-width': strokeWidth * 2,
-    //         class: [color]
-    //       },
-    //       text: {
-    //         style: {
-    //           'font-size': `${fontSize}px`,
-    //           'text-anchor': i <= 2 ? 'end' : 'start'
-    //         },
-    //         transform: `translate(${i <= 2 ? -5 : 5} 0)`,
-    //         'stroke-width': strokeWidth * 2,
-    //         class: [color]
-    //       }
-    //     }
-    //   })
-    // },
     strokeWidth () {
       return (100 / this.width) * 2
     },
@@ -237,7 +113,6 @@ export default {
     }
   },
   mounted () {
-    this.$emit('setMaxWidth', 320)
     this.setWidth()
   },
   methods: {
@@ -245,7 +120,6 @@ export default {
       this.width = 0
       this.$nextTick(() => {
         this.width = this.$refs.cycle.getBoundingClientRect().width - 90
-        this.$emit('setHeight', this.width)
       })
     }
   }
