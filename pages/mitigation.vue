0b561e31422eb216443cb72291851eeb8be364c5
Fidel Thomet <me@fidelthomet.com>
2018-09-17 16:30:23 +0200

    [Typo-Fix] mitigation: superscript

--- pages/mitigation.vue
+++ pages/mitigation.vue
@@ -2,7 +2,7 @@
   <section class="wrapper">
     <h2 class="h1">Mitigation</h2>
 
-    <p>Globally enforced emission policies are a means to reach global warming targets. This is where representative concentration pathways (RCPs) come in. Again there are five scenarios, ranging from RCP 1.9 to RCP 8.5. The value refers to the "radiative forcing", a quantity that is crucially dependent on GHG concentrations and which, in turn, determines the atmosphere's temperature. More specifically, the numbers give the change in radiative forcing in 2100 compared to preindustrial times. I.e. in the RCP2.6 scenario the globally/annually averaged radiative forcing will have risen by 2.6 W/m^2 compared to preindustrial times.</p>
+    <p>Globally enforced emission policies are a means to reach global warming targets. This is where representative concentration pathways (RCPs) come in. Again there are five scenarios, ranging from RCP 1.9 to RCP 8.5. The value refers to the "radiative forcing", a quantity that is crucially dependent on GHG concentrations and which, in turn, determines the atmosphere's temperature. More specifically, the numbers give the change in radiative forcing in 2100 compared to preindustrial times. I.e. in the RCP2.6 scenario the globally/annually averaged radiative forcing will have risen by 2.6 W/m<sup>2</sup> compared to preindustrial times.</p>
 
     <table class="no-margin">
       <thead>
@@ -16,37 +16,37 @@
       <tbody>
         <tr>
           <td> 1.9 </td>
-          <td> 1.9 W/m2 </td>
+          <td> 1.9 W/m<sup>2</sup> </td>
           <td> ~1.5°C </td>
           <td> Very Strongly Declining Emissions </td>
         </tr>
         <tr>
           <td> 2.6 </td>
-          <td> 2.6 W/m2 </td>
+          <td> 2.6 W/m<sup>2</sup> </td>
           <td> ~2.0°C </td>
           <td> Strongly Declining Emissions </td>
         </tr>
         <tr>
           <td> 4.5 </td>
-          <td> 4.5 W/m2 </td>
+          <td> 4.5 W/m<sup>2</sup> </td>
           <td> ~2.4°C </td>
           <td> Slowly Declining Emissions </td>
         </tr>
         <tr>
           <td> 6.0 </td>
-          <td> 6.0 W/m2 </td>
+          <td> 6.0 W/m<sup>2</sup> </td>
           <td> ~2.8°C </td>
           <td> Stabilising Emissions </td>
         </tr>
         <tr>
           <td> 8.5 </td>
-          <td> 8.5 W/m2 </td>
+          <td> 8.5 W/m<sup>2</sup> </td>
           <td> ~4.3°C </td>
           <td> Rising Emissions </td>
         </tr>
       </tbody>
     </table>
-    <p>The Paris Agreement aims to keep the global mean temperature increase below 2°C. This corresponds to a radiative forcing increase of 2.6 W/m2 or below, i.e. to RCP 1.9 or RCP 2.6.</p>
+    <p>The Paris Agreement aims to keep the global mean temperature increase below 2°C. This corresponds to a radiative forcing increase of 2.6 W/m<sup>2</sup> or below, i.e. to RCP 1.9 or RCP 2.6.</p>
 
     <p>Until now we only looked at SSP-Baseline Scenarios where we assume that the world will develop like business-as-usual in the future. Negative impacts of climate change are ignored. Let&#8217;s now set emission goals and include RCPs.</p>
 
@@ -55,11 +55,13 @@
         slot="left"
         :max="3200"
         legend="rcp"
+        both-legends
         scenario="carbonSsp1"/>
       <VisChart
         slot="right"
         :max="3200"
         legend="rcp"
+        both-legends
         scenario="carbonSsp5"/>
     </LayoutColumns>
 
@@ -68,45 +70,26 @@
     <LayoutColumns class="extended">
       <VisChart
         slot="left"
+        :dynamic-filter="[ssp, hoverSSP]"
+        :legend-filter="ssp"
         :max="1200"
-        dynamic-filter="SSP1"
         legend="rcp"
         scenario="finalenergy"/>
       <VisChart
         slot="right"
+        :dynamic-filter="[ssp, hoverSSP]"
+        :legend-filter="ssp"
         :max="140000"
-        dynamic-filter="SSP1"
+        legend="rcp"
+        hide-legend
         scenario="emissions"/>
     </LayoutColumns>
 
-    <p class="ssp-selection">
-      <InHighlight
-        :out="false"
-        init-active
-        field="charts.filter"
-        class="green"
-        value="SSP1">SSP1</InHighlight>
-      <InHighlight
-        :out="false"
-        field="charts.filter"
-        class="blue"
-        value="SSP2">SSP2</InHighlight>
-      <InHighlight
-        :out="false"
-        field="charts.filter"
-        class="red"
-        value="SSP3">SSP3</InHighlight>
-      <InHighlight
-        :out="false"
-        field="charts.filter"
-        class="yellow"
-        value="SSP4">SSP4</InHighlight>
-      <InHighlight
-        :out="false"
-        field="charts.filter"
-        class="violet"
-        value="SSP5">SSP5</InHighlight>
-    </p>
+    <LayoutRadioGroup
+      :options="ssps"
+      v-model="ssp"
+      class="center"
+      @hover="setHoverSSP"/>
     <p>
       Compared to the Baseline-Scenarios this results in some reductions in final energy and substantially decreased emissions.
     </p>
@@ -118,7 +101,37 @@
 
 <script>
 export default {
-  components: {
+  data () {
+    return {
+      ssp: 'SSP1',
+      hoverSSP: null,
+      ssps: [{
+        label: 'SSP1',
+        value: 'SSP1',
+        color: 'green'
+      }, {
+        label: 'SSP2',
+        value: 'SSP2',
+        color: 'blue'
+      }, {
+        label: 'SSP3',
+        value: 'SSP3',
+        color: 'red'
+      }, {
+        label: 'SSP4',
+        value: 'SSP4',
+        color: 'yellow'
+      }, {
+        label: 'SSP5',
+        value: 'SSP5',
+        color: 'violet'
+      }]
+    }
+  },
+  methods: {
+    setHoverSSP (e) {
+      this.hoverSSP = e
+    }
   }
 }
 </script>
@@ -126,7 +139,7 @@ export default {
 <style scoped lang="scss">
 @import "~@/assets/style/global";
 .wrapper {
-  .ssp-selection {
+  .LayoutRadioGroup {
     margin-top: $spacing * 0.5;
   }
 }
