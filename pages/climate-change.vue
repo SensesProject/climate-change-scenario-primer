2691f3695a789710a5e75b5b31151eed8ab0b118
Fidel Thomet <me@fidelthomet.com>
2018-09-17 14:44:13 +0200

    [Update] Climate: Content & Map improvements

--- pages/climate-change.vue
+++ pages/climate-change.vue
@@ -1,41 +1,40 @@
-<template>
-  <section class="wrapper">
-    <h2>Climate</h2>
+<template lang="pug">
+  section.wrapper
+    h2.h1 Climate
+    p The scenarios discussed above assume different green house gas (GHG) emissions into the atmosphere. Not not all GHG emitted into the atmosphere remain there, however. For instance, the oceans absorb large parts of atmospheric CO2 and vegetation can act as a carbon sink, as well. Still, the sheer magnitude of the GHG emitted in the past causes GHG to accumulate in the atmosphere and thus rising GHG  concentrations.
 
-    <p>The scenarios discussed above lead to particular concentrations of green house gases (GHG) in the atmosphere. Climate models then simulate the atmosphere and translate these GHG concentrations to meteorological quantities such as temperature or precipitation. Temperature and precipitation are the two most important quantities in climate impact research. It turns out that many more systems than just the atmosphere decisively influence the climate. Therefore more and more subsystems, e.g. oceans, vegetation or sea ice, had to be added to climate models. Because of that these models are also known as "Earth System Models".</p>
-    <LayoutColumns
-      class="extended"
-      left-title="RCP 2.6"
-      right-title="RCP 6.0">
-      <VisMap
-        slot="left"
-        :grid="rcp26"
-        map-id="map1"
-        projection="EPSG:54030"
-        color-scale="interpolateRdBu"
-        invert-scale
-        static/>
-      <VisMap
-        slot="right"
-        :grid="rcp60"
-        map-id="map2"
-        projection="EPSG:54030"
-        color-scale="interpolateRdBu"
-        invert-scale
-        static/>
-    </LayoutColumns>
-    <InLegendSequential
-      :domain="[0, 25]"
-      :limit-range="[-2, 12]"
-      invert-scale/>
-    <div class="map-description">Change in average near surface temperature (2006–2010 to 2095–2099)</div>
-    <p>The above plot shows the change in average near surface temperature from 2006–2010 to 2095–2099 for two different GHG scenarios: RCP 2.6 and RCP 6.0. As you can see: the overall temperature is going to raise, the intensity varies between regions and the temperature changes in the RCP 6.0 scenario are far more severe.</p>
+    p GHG, as hinted by their name, play the key role in the green house effect. The  green house effect leads to rising temperatures and can be quantified by the so called "radiative forcing". A higher radiative forcing leads to higher atmospheric temperatures. The number in the RCP scenario is related to this. The radiative forcing in RCP8.5, for instance, is in 2100 8.5 W/m² higher than during preindustrial times.
 
-    <LayoutRecap>
-      <li>Climate models are complex systems describing the physical world including its atmosphere.</li>
-      <li>Climate models generate projections on future temperature and precipitation based on GHG concentrations.</li>
-    </LayoutRecap>
-  </section>
+    LayoutColumns.extended(left-title="RCP 2.6", right-title="RCP 6.0")
+      VisMap(
+        slot="left",
+        :grid="rcp26",
+        map-id="map1",
+        projection="EPSG:54030",
+        color-scale="interpolateRdBu",
+        invert-scale,
+        static)
+      VisMap(
+        slot="right",
+        :grid="rcp60",
+        map-id="map2",
+        projection="EPSG:54030",
+        color-scale="interpolateRdBu",
+        invert-scale,
+        static)
+    InLegendSequential.center(
+      :domain="[0, 25]",
+      :limit-range="[-2, 12]",
+      invert-scale)
+    div.map-description.center Change in average near surface temperature
+      span  (2006–2010&nbspto&nbsp2095–2099)
+    p Climate models then simulate the atmosphere and translate these GHG  concentrations to meteorological quantities such as temperature or precipitation. Temperature and precipitation are the two most important quantities in climate impact research. It turns out that many more systems than just the atmosphere decisively influence the climate. Therefore more and more subsystems, e.g. oceans, vegetation or sea ice, had to be added to climate models. Because of that these models are also known as "Earth System Models".
+
+    p The above plot shows the change in average near surface temperature from 2006- 2010 to 2095-2099 for two different GHG scenarios: RCP 2.6 and RCP 6.0. As you can see: the overall temperature is going to raise, the intensity varies between regions and the temperature increases in the RCP 6.0 scenario are far more severe.
+
+    LayoutRecap
+      li Climate models are complex systems describing the physical world including its atmosphere.
+      li Climate models generate projections on future temperature and precipitation based on GHG concentrations.
 </template>
 
 <script>
