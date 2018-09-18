23adcfcbbb363923b1ffa92986236d9d237c2a83
Fidel Thomet <me@fidelthomet.com>
2018-09-18 10:23:36 +0200

    [Update] SEO; resolves #27

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,17 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Primer on climate change scenarios' },
+      { property: 'og:image:width', content: '1190' },
+      { property: 'og:image:height', content: '623' },
+      { property: 'og:title', content: 'Climate Change Scenario Primer' },
+      { property: 'og:description', content: 'We can’t predict the future, but scenarios allow us to explore possible futures. This primer explains what climate change scenarios are and how they are connected to socioeconomics, energy & land use, emissions, climate and climate impacts.' },
+      { property: 'og:url', content: 'http://climatescenarios.org/primer' },
+      { property: 'og:image', content: 'http://climatescenarios.org/primer/og.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@UCLab_Potsdam' },
+      { name: 'twitter:title', content: 'Climate Change Scenario Primer' },
+      { name: 'twitter:description', content: 'We can&rsquo;t predict the future, but scenarios allow us to explore possible futures. This primer explains what climate change scenarios are and how they are connected to socioeconomics, energy &amp; land use, emissions, climate and climate impacts.' },
+      { name: 'twitter:image', content: 'http://climatescenarios.org/primer/og.png' },
       ...msapplication()
     ],
     link: [
