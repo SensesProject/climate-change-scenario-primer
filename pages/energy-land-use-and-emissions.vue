5fe314d271a38b1e1648a3db8853a8ead7c0e9e6
Jonas <jonasparnow@gmail.com>
2018-09-27 12:03:10 +0200

    Elmar corrections 3

--- pages/energy-land-use-and-emissions.vue
+++ pages/energy-land-use-and-emissions.vue
@@ -7,7 +7,7 @@
 
     <LayoutIAMs/>
 
-    <p>So feeding socioeconomic scenario assumptions into IAMs will get us scenarios for energy use, land use and emissions. Let&#8217;s take a look how this plays out for the socioeconomic futures described by the SSPs.</p>
+    <p>So feeding socioeconomic scenario assumptions into IAMs will get us scenarios for energy use, land use and emissions. Let&#8217;s take a look how this plays out for the socioeconomic futures described by the SSPs. Population and GDP developments in the SSPs are shown below.</p>
 
     <LayoutColumns class="extended">
       <VisChart
@@ -21,21 +21,28 @@
         scenario="gdp"/>
     </LayoutColumns>
 
-    <p>
-      Assumptions on population growth and GDP are already part of the SSP scenario assumptions. This shows us how they turn out over time and in numbers.
-      As you can see the
+    <p>As you can see the
       <InHighlight
         field="charts.ssp"
         class="red"
-        value="SSP3">SSP&#8239;3</InHighlight> scenario of regional rivalry leads to continued population growth but relatively low growth in GDP. In contrast,
+        value="SSP3">
+        SSP&#8239;3
+      </InHighlight>
+      scenario of regional rivalry leads to continued population growth but relatively low growth in GDP. In contrast,
       <InHighlight
         field="charts.ssp"
         class="green"
-        value="SSP1">SSP&#8239;1</InHighlight> (Sustainability) and
+        value="SSP1">
+        SSP&#8239;1
+      </InHighlight>
+      (Sustainability) and
       <InHighlight
         field="charts.ssp"
         class="violet"
-        value="SSP5">SSP&#8239;5</InHighlight> (Fossil-fuelled development) show a peak and decline in population growth, but much higher GDP growth. Although they have a similar pattern of population growth, they show significantly different levels of GDP growth.
+        value="SSP5">
+        SSP&#8239;5
+      </InHighlight>
+      (Fossil-fuelled development) show a peak and decline in population growth, but much higher GDP growth. Although they have a similar pattern of population growth, they show significantly different levels of GDP growth.
     </p>
 
     <LayoutColumns class="extended">
@@ -53,10 +60,14 @@
     </LayoutColumns>
 
     <p>
-      This and other factors such as technological progress and lifestyles affect future energy and land use. Here we show the examples of energy demand and CO2 emissions. The resource intensity of <InHighlight
+      This and other factors such as technological progress and lifestyles affect future energy and land use. Here we show the examples of energy demand and CO<sub>2</sub> emissions. The resource intensity of
+      <InHighlight
         field="charts.ssp"
         class="violet"
-        value="SSP5">SSP&#8239;5</InHighlight> combined with the high GDP growth comes at a cost: High energy needs and lots of CO<sub>2</sub> emissions.
+        value="SSP5">
+        SSP&#8239;5
+      </InHighlight>
+      combined with the high GDP growth comes at a cost: High energy needs and lots of CO<sub>2</sub> emissions.
     </p>
     <LayoutRecap>
       <li>Integrated assessment models (IAMs) are complex models of the energy-land-economy-climate system that use socioeconomic assumptions to produce energy, land use and emissions scenarios.</li>
