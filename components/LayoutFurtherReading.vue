7fa2bbf8ce3a3182529a16ead865a50f95c657d0
Fidel Thomet <me@fidelthomet.com>
2018-11-26 15:24:16 +0100

    [Update] added further reading component

---
+++ components/LayoutFurtherReading.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  div.LayoutFurtherReading
+    div.further-reading
+      h3 Further reading
+      ul
+        slot
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/style/global";
+.LayoutFurtherReading {
+  h3 {
+    margin-bottom: $spacing / 4;
+  }
+
+  ul {
+    list-style: url(~/assets/img/li-black.svg);
+
+    li + li {
+      margin-top: $spacing / 4;
+    }
+  }
+
+  margin-bottom: $spacing / 2;
+}
+</style>
