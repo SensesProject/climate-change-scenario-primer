a8488d7285810fdf8670b50928212c7554e2bc11
Fidel <fidel@air.local>
2019-07-08 13:54:34 +0200

    [Update] added version history

---
+++ pages/versions.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  section.wrapper
+    h2.h1 Version History
+    h3.h2
+      a(href="https://github.com/SensesProject/climate-change-scenario-primer/releases/tag/1.2.2") 1.2.2
+    ul
+      li Added version history
+    h3.h2
+      a(href="https://github.com/SensesProject/climate-change-scenario-primer/releases/tag/1.2.1") 1.2.1
+    ul
+      li Added RCP 1.9 scenario data
+      li Removed ambiguous indicator from IAM comparison chart
+    h3.h2
+      a(href="https://github.com/SensesProject/climate-change-scenario-primer/releases/tag/1.2.0") 1.2.0
+    ul
+      li Added support for printing
+      li Added support for Internet Explorer
+    h3.h2
+      a(href="https://github.com/SensesProject/climate-change-scenario-primer/releases/tag/1.1.0") 1.1.0
+    ul
+      li Added references for further reading
+      li Minor fixes and improvements
+    h3.h2
+      a(href="https://github.com/SensesProject/climate-change-scenario-primer/releases/tag/1.0.0") 1.0.0
+    ul
+      li Initial release
+    h3.h2
+      a(href="https://github.com/SensesProject/climate-change-scenario-primer/releases/tag/1.0.0-Beta") 1.0.0-Beta
+    ul
+      li Beta-Version
+</template>
+
+<script>
+export default { }
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/style/global";
+
+ul {
+  list-style: url(~/assets/img/li-violet.svg);
+  line-height: 1.4;
+  margin: $spacing / 4 0 $spacing / 2;
+}
+</style>
