1b1d8993c9ac8ee37a367669377bcebe5f36f83a
Fidel Thomet <me@fidelthomet.com>
2020-04-06 10:33:59 +0200

    [chore] removed favicon

--- assets/js/favicon.js
+++ assets/js/favicon.js
@@ -10,12 +10,12 @@ module.exports = {
       { rel: 'apple-touch-icon-precomposed', sizes: '60x60', href: './apple-touch-icon-60x60.png' },
       { rel: 'apple-touch-icon-precomposed', sizes: '120x120', href: './apple-touch-icon-120x120.png' },
       { rel: 'apple-touch-icon-precomposed', sizes: '76x76', href: './apple-touch-icon-76x76.png' },
-      { rel: 'apple-touch-icon-precomposed', sizes: '152x152', href: './apple-touch-icon-152x152.png' },
-      { rel: 'icon', type: 'image/png', href: './favicon-196x196.png', sizes: '196x196' },
-      { rel: 'icon', type: 'image/png', href: './favicon-96x96.png', sizes: '96x96' },
-      { rel: 'icon', type: 'image/png', href: './favicon-32x32.png', sizes: '32x32' },
-      { rel: 'icon', type: 'image/png', href: './favicon-16x16.png', sizes: '16x16' },
-      { rel: 'icon', type: 'image/png', href: './favicon-128.png', sizes: '128x128' }
+      { rel: 'apple-touch-icon-precomposed', sizes: '152x152', href: './apple-touch-icon-152x152.png' }
+      // { rel: 'icon', type: 'image/png', href: './favicon-196x196.png', sizes: '196x196' },
+      // { rel: 'icon', type: 'image/png', href: './favicon-96x96.png', sizes: '96x96' },
+      // { rel: 'icon', type: 'image/png', href: './favicon-32x32.png', sizes: '32x32' },
+      // { rel: 'icon', type: 'image/png', href: './favicon-16x16.png', sizes: '16x16' },
+      // { rel: 'icon', type: 'image/png', href: './favicon-128.png', sizes: '128x128' }
     ]
   },
   msapplication: function () {
