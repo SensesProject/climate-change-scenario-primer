de805c49bc9d2a457e7ec40da7f29f9b44f8a6b8
Fidel Thomet <me@fidelthomet.com>
2020-05-28 10:46:55 +0200

    [chore] updated meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
-const { favicon, msapplication } = require('./assets/js/favicon.js')
+// const { favicon, msapplication } = require('./assets/js/favicon.js')
+const { getHead } = require('library/src/assets/js/head.js')
 
 module.exports = {
   /*
@@ -15,27 +16,11 @@ module.exports = {
     htmlAttrs: {
       lang: 'en-US'
     },
-    title: 'Climate Change Scenario',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Primer on climate change scenarios' },
-      { property: 'og:image:width', content: '1190' },
-      { property: 'og:image:height', content: '623' },
-      { property: 'og:title', content: 'Climate Change Scenario Primer' },
-      { property: 'og:description', content: 'We can’t predict the future, but scenarios allow us to explore possible futures. This primer explains what climate change scenarios are and how they are connected to socioeconomics, energy & land use, emissions, climate and climate impacts.' },
-      { property: 'og:url', content: 'http://climatescenarios.org/primer' },
-      { property: 'og:image', content: 'http://climatescenarios.org/primer/og.png' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:site', content: '@UCLab_Potsdam' },
-      { name: 'twitter:title', content: 'Climate Change Scenario Primer' },
-      { name: 'twitter:description', content: 'We can&rsquo;t predict the future, but scenarios allow us to explore possible futures. This primer explains what climate change scenarios are and how they are connected to socioeconomics, energy &amp; land use, emissions, climate and climate impacts.' },
-      { name: 'twitter:image', content: 'http://climatescenarios.org/primer/og.png' },
-      ...msapplication()
-    ],
-    link: [
-      ...favicon()
-    ]
+    ...getHead({
+      title: 'Primer to Climate Scenarios',
+      description: 'An interactive primer that gives an introduction to climate change scenarios are and how they are connected to socioeconomics, energy & land use, emissions, climate change and climate impacts.',
+      image: 'https://climatescenarios.org/share/og-primer.jpg'
+    })
   },
   css: [
     // Load a node module directly (here it's a SASS file)
